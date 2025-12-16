60a7cf6b7cf0fe0be26e4a7dbd0f8d1bf483c548
Matěj Kulíšek <matej.kulisek99@icloud.com>
2025-12-16 08:33:39 +0100

    Rodič.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Soubor: main.cpp
+#include "Formule.h" // Tímto načteš kód z druhého souboru
+
+int main() {
+    Ferrari mojeAuto;
+
+    mojeAuto.zvukMotoru();
+    mojeAuto.zrychli();
+
+    return 0;
+}
